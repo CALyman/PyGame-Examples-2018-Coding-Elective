920b81de3d16bcaa75c2ed12e59e9889f6138da9
CALyman <36332417+CALyman@users.noreply.github.com>
2018-02-28 14:10:03 -0500

    Update Sprite_Skeleton.py

--- Sprite_Skeleton.py
+++ Sprite_Skeleton.py
@@ -1,8 +1,8 @@
 import pygame
 from pygame.locals import *
 
-class User_Sprite(pygame.sprite.Sprite):
-    #Starting position and speed
+class User_Sprite(pygame.sprite.Sprite): #User_Sprite can be replaced with the name of your sprite
+    #Starting positions
     x = 250
     y = 250
 
@@ -16,7 +16,7 @@ class User_Sprite(pygame.sprite.Sprite):
         self.image.set_colorkey((255, 255, 255))
 
         #Create an image and give it a png picture
-        self.image = pygame.image.load("images/red_mushroom.png").convert_alpha()
+        self.image = pygame.image.load("images/red_mushroom.png").convert_alpha() #assigning the image to the variable self.image
         
         #Create a rect with the size of the image and center it on it's location
         self.rect = self.image.get_rect(center=(self.x, self.y))
